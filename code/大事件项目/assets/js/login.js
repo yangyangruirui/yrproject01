ae7eea4f408d27275fa2ee1f123463d890a3f0b4
yangrui <514303840@qq.com>
2022-01-13 17:32:05 +0800

    登录与注册前端验证

--- code/大事件项目/assets/js/login.js
+++ code/大事件项目/assets/js/login.js
@@ -10,4 +10,19 @@ $(function() {
         $('#login-box').show();
         $('#reg-box').hide();
     })
+
+    //自定义验证规则
+    var form = layui.form;
+    form.verify({
+        //定义密码验证规则
+        pwd: [
+            /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
+        ],
+        repwd: function(value) {
+            var pwdval = $("#pwd").val();
+            if (pwdval !== value) {
+                return "两次输入的密码不一致！";
+            }
+        }
+    })
 })
